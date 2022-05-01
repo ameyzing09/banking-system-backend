825ba08bf70ce6e7a2af72ad4893d937c69667f5
= <=>
2022-05-01 10:50:20 +0530

    added incremental script

--- scripts/db-migrate-script-0.0.2.sql
+++ scripts/db-migrate-script-0.0.2.sql
@@ -1,3 +1,7 @@
 ALTER TABLE `banking-system`.`transaction` 
 ADD COLUMN `transaction_amount` FLOAT NOT NULL AFTER `transaction_description`,
 CHANGE COLUMN `transaction_description` `transaction_description` VARCHAR(255) NOT NULL AFTER `account_id`;
+
+ALTER TABLE `banking-system`.`transaction` 
+ADD COLUMN `balance` FLOAT NOT NULL AFTER `transaction_amount`,
+CHANGE COLUMN `transaction_date` `transaction_date` DATETIME NOT NULL ;
